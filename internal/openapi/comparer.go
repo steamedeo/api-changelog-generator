7db1caf929ec801378c4394b29eadff2090b45d9
Stefano <ste.amedeo@gmail.com>
2026-01-09 09:39:02 +0100

    v0.0.4: improved quality of output changelog

--- internal/openapi/comparer.go
+++ internal/openapi/comparer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/pb33f/libopenapi"
@@ -30,62 +31,235 @@ func (c *Comparer) compare() error {
 }
 
 func (c *Comparer) formatChange(change *model.Change) string {
-	var desc string
 	property := change.Property
+
+	// Helper to format text values - no truncation, show everything
+	formatValue := func(val string) string {
+		// Skip if empty
+		if val == "" {
+			return ""
+		}
+
+		// Replace newlines with spaces for single-line display
+		return strings.ReplaceAll(val, "\n", " ")
+	}
 
 	// Make property names more human-readable
 	switch property {
 	case "codes":
 		if change.New != "" {
-			desc = fmt.Sprintf("Response code `%s` added", change.New)
+			return fmt.Sprintf("Response code `%s` added", change.New)
 		} else if change.Original != "" {
-			desc = fmt.Sprintf("Response code `%s` removed", change.Original)
+			return fmt.Sprintf("Response code `%s` removed", change.Original)
 		}
 	case "parameters":
 		if change.New != "" {
-			desc = fmt.Sprintf("Parameter `%s` added", change.New)
+			return fmt.Sprintf("Parameter `%s` added", change.New)
 		} else if change.Original != "" {
-			desc = fmt.Sprintf("Parameter `%s` removed", change.Original)
+			return fmt.Sprintf("Parameter `%s` removed", change.Original)
 		}
 	case "properties":
+		// Note: context must be added by caller - this is just the base message
 		if change.New != "" {
-			desc = fmt.Sprintf("Property `%s` added to schema", change.New)
+			return fmt.Sprintf("Property `%s` added", change.New)
 		} else if change.Original != "" {
-			desc = fmt.Sprintf("Property `%s` removed from schema", change.Original)
+			return fmt.Sprintf("Property `%s` removed", change.Original)
 		}
 	case "schemas":
 		if change.New != "" {
-			desc = fmt.Sprintf("Schema `%s` added", change.New)
+			return fmt.Sprintf("Schema `%s` added", change.New)
 		} else if change.Original != "" {
-			desc = fmt.Sprintf("Schema `%s` removed", change.Original)
+			return fmt.Sprintf("Schema `%s` removed", change.Original)
 		}
 	case "deprecated":
 		if change.New == "true" {
-			desc = "Endpoint marked as deprecated"
+			return "Marked as deprecated"
 		} else {
-			desc = "Endpoint no longer deprecated"
+			return "No longer deprecated"
 		}
 	case "version":
-		desc = fmt.Sprintf("API version updated from `%s` to `%s`", change.Original, change.New)
+		return fmt.Sprintf("API version updated from `%s` to `%s`", change.Original, change.New)
+	case "description":
+		// Special handling for description changes
+		oldDesc := formatValue(change.Original)
+		newDesc := formatValue(change.New)
+		return fmt.Sprintf("Description updated:\n  - Old: %s\n  - New: %s", oldDesc, newDesc)
+	case "summary":
+		oldSummary := formatValue(change.Original)
+		newSummary := formatValue(change.New)
+		return fmt.Sprintf("Summary updated from '%s' to '%s'", oldSummary, newSummary)
+	case "$ref", "reference":
+		if change.New != "" && change.Original != "" {
+			// Skip if values are the same
+			if change.New == change.Original {
+				return ""
+			}
+			return fmt.Sprintf("Reference changed from `%s` to `%s`", change.Original, change.New)
+		} else if change.New != "" {
+			return fmt.Sprintf("Reference set to `%s`", change.New)
+		} else {
+			return fmt.Sprintf("Reference `%s` removed", change.Original)
+		}
+	case "url":
+		if change.New != "" && change.Original != "" {
+			return fmt.Sprintf("URL changed from `%s` to `%s`", change.Original, change.New)
+		}
+	case "type":
+		if change.New != "" && change.Original != "" {
+			return fmt.Sprintf("Type changed from `%s` to `%s`", change.Original, change.New)
+		}
+	case "required":
+		if change.New == "true" && change.Original == "false" {
+			return "Now required"
+		} else if change.New == "false" && change.Original == "true" {
+			return "No longer required"
+		}
+	case "format":
+		if change.New != "" && change.Original != "" {
+			return fmt.Sprintf("Format changed from `%s` to `%s`", change.Original, change.New)
+		}
 	default:
 		// Handle paths (endpoints)
 		if len(property) > 0 && property[0] == '/' {
-			desc = fmt.Sprintf("Endpoint `%s` added", property)
-		} else {
-			// Generic fallback
+			return fmt.Sprintf("Endpoint `%s` added", property)
+		} else if strings.HasPrefix(property, "x-") {
+			// Extension property
 			if change.Original != "" && change.New != "" {
-				desc = fmt.Sprintf("%s: changed from `%s` to `%s`", property, change.Original, change.New)
+				return fmt.Sprintf("Extension `%s` modified", property)
 			} else if change.New != "" {
-				desc = fmt.Sprintf("%s: `%s`", property, change.New)
-			} else if change.Original != "" {
-				desc = fmt.Sprintf("%s: was `%s`", property, change.Original)
+				return fmt.Sprintf("Extension `%s` added", property)
 			} else {
-				desc = property
+				return fmt.Sprintf("Extension `%s` removed", property)
+			}
+		} else {
+			// Generic fallback with better formatting
+			if change.Original != "" && change.New != "" {
+				// Skip if values are the same
+				if change.Original == change.New {
+					return ""
+				}
+				oldVal := formatValue(change.Original)
+				newVal := formatValue(change.New)
+				return fmt.Sprintf("`%s` changed from '%s' to '%s'", property, oldVal, newVal)
+			} else if change.New != "" {
+				newVal := formatValue(change.New)
+				return fmt.Sprintf("`%s` set to '%s'", property, newVal)
+			} else if change.Original != "" {
+				oldVal := formatValue(change.Original)
+				return fmt.Sprintf("`%s` removed (was '%s')", property, oldVal)
+			} else {
+				return fmt.Sprintf("`%s` modified", property)
 			}
 		}
 	}
 
+	return ""
+}
+
+// Helper function to format a change with context
+func (c *Comparer) formatChangeWithContext(change *model.Change, context string) string {
+	desc := c.formatChange(change)
+	if desc == "" {
+		return ""
+	}
+
+	// Always prepend context if provided and not already in the description
+	if context != "" && !strings.Contains(desc, context) {
+		return fmt.Sprintf("**%s**: %s", context, desc)
+	}
 	return desc
+}
+
+// Process operation changes with endpoint context
+func (c *Comparer) processOperationChangesWithContext(endpoint string, opChanges *model.OperationChanges,
+	added, modified, removed, breaking *[]string, seen map[string]bool) {
+
+	if opChanges == nil {
+		return
+	}
+
+	// Get all changes from this operation
+	changes := opChanges.GetAllChanges()
+	for _, change := range changes {
+		// Create unique key for deduplication based on change properties
+		changeKey := fmt.Sprintf("%d|%s|%s|%s", change.ChangeType, change.Property, change.Original, change.New)
+		if seen[changeKey] {
+			continue
+		}
+		seen[changeKey] = true
+
+		changeDesc := c.formatChangeWithContext(change, endpoint)
+
+		// Skip empty descriptions
+		if changeDesc == "" {
+			continue
+		}
+
+		// Categorize
+		isActuallyBreaking := change.Breaking &&
+			(change.ChangeType == model.ObjectRemoved ||
+				change.ChangeType == model.PropertyRemoved ||
+				(change.Property == "required" && change.New == "true" && change.Original == "false"))
+
+		if isActuallyBreaking {
+			*breaking = append(*breaking, changeDesc)
+		}
+
+		switch change.ChangeType {
+		case model.ObjectAdded, model.PropertyAdded:
+			*added = append(*added, changeDesc)
+		case model.Modified:
+			*modified = append(*modified, changeDesc)
+		case model.ObjectRemoved, model.PropertyRemoved:
+			*removed = append(*removed, changeDesc)
+		}
+	}
+}
+
+// Process schema changes with schema context
+func (c *Comparer) processSchemaChangesWithContext(schemaName string, schemaChanges *model.SchemaChanges,
+	added, modified, removed, breaking *[]string, seen map[string]bool) {
+
+	if schemaChanges == nil {
+		return
+	}
+
+	// Get all changes from this schema
+	changes := schemaChanges.GetAllChanges()
+	for _, change := range changes {
+		// Create unique key for deduplication based on change properties
+		changeKey := fmt.Sprintf("%d|%s|%s|%s", change.ChangeType, change.Property, change.Original, change.New)
+		if seen[changeKey] {
+			continue
+		}
+		seen[changeKey] = true
+
+		changeDesc := c.formatChangeWithContext(change, schemaName)
+
+		// Skip empty descriptions
+		if changeDesc == "" {
+			continue
+		}
+
+		// Categorize
+		isActuallyBreaking := change.Breaking &&
+			(change.ChangeType == model.ObjectRemoved ||
+				change.ChangeType == model.PropertyRemoved ||
+				(change.Property == "required" && change.New == "true" && change.Original == "false"))
+
+		if isActuallyBreaking {
+			*breaking = append(*breaking, changeDesc)
+		}
+
+		switch change.ChangeType {
+		case model.ObjectAdded, model.PropertyAdded:
+			*added = append(*added, changeDesc)
+		case model.Modified:
+			*modified = append(*modified, changeDesc)
+		case model.ObjectRemoved, model.PropertyRemoved:
+			*removed = append(*removed, changeDesc)
+		}
+	}
 }
 
 func (c *Comparer) GenerateChangelog() error {
@@ -94,7 +268,6 @@ func (c *Comparer) GenerateChangelog() error {
 	}
 	if c.totalChanges > 0 {
 		date := time.Now().Format("2006-01-02")
-		allChanges := c.changes.GetAllChanges()
 
 		// Get API version from the document info
 		apiVersion := "Unknown"
@@ -103,16 +276,147 @@ func (c *Comparer) GenerateChangelog() error {
 			apiVersion = v3Model.Model.Info.Version
 		}
 
-		// Group changes by type
+		// Separate endpoint additions/removals from other changes
+		endpointAdditions := []string{}
+		endpointRemovals := []string{}
 		added := []string{}
 		modified := []string{}
 		removed := []string{}
 		breaking := []string{}
 
+		// Track seen changes to avoid duplicates
+		seen := make(map[string]bool)
+
+		// Process path changes with context
+		if c.changes.PathsChanges != nil && c.changes.PathsChanges.PathItemsChanges != nil {
+			for path, pathItemChanges := range c.changes.PathsChanges.PathItemsChanges {
+				// Check each HTTP method
+				methods := []struct {
+					name    string
+					changes *model.OperationChanges
+				}{
+					{"GET", pathItemChanges.GetChanges},
+					{"POST", pathItemChanges.PostChanges},
+					{"PUT", pathItemChanges.PutChanges},
+					{"DELETE", pathItemChanges.DeleteChanges},
+					{"PATCH", pathItemChanges.PatchChanges},
+					{"OPTIONS", pathItemChanges.OptionsChanges},
+					{"HEAD", pathItemChanges.HeadChanges},
+					{"TRACE", pathItemChanges.TraceChanges},
+				}
+
+				for _, method := range methods {
+					if method.changes != nil {
+						endpoint := method.name + " " + path
+						c.processOperationChangesWithContext(endpoint, method.changes, &added, &modified, &removed, &breaking, seen)
+					}
+				}
+			}
+		}
+
+		// Process component/schema changes with context
+		if c.changes.ComponentsChanges != nil && c.changes.ComponentsChanges.SchemaChanges != nil {
+			for schemaName, schemaChanges := range c.changes.ComponentsChanges.SchemaChanges {
+				context := "Schema `" + schemaName + "`"
+				c.processSchemaChangesWithContext(context, schemaChanges, &added, &modified, &removed, &breaking, seen)
+			}
+		}
+
+		// Process component-level changes (parameters, responses, examples, etc.)
+		if c.changes.ComponentsChanges != nil {
+			componentChanges := c.changes.ComponentsChanges.GetAllChanges()
+			for _, change := range componentChanges {
+				// Create unique key for deduplication
+				changeKey := fmt.Sprintf("%d|%s|%s|%s", change.ChangeType, change.Property, change.Original, change.New)
+				if seen[changeKey] {
+					continue
+				}
+				seen[changeKey] = true
+
+				// Add context to indicate these are reusable component definitions
+				var context string
+				switch change.Property {
+				case "parameters":
+					context = "Components"
+				case "responses":
+					context = "Components"
+				case "examples":
+					context = "Components"
+				case "requestBodies":
+					context = "Components"
+				case "headers":
+					context = "Components"
+				case "links":
+					context = "Components"
+				case "callbacks":
+					context = "Components"
+				default:
+					context = "Components"
+				}
+
+				changeDesc := c.formatChangeWithContext(change, context)
+				if changeDesc == "" {
+					continue
+				}
+
+				// Categorize
+				isActuallyBreaking := change.Breaking &&
+					(change.ChangeType == model.ObjectRemoved ||
+						change.ChangeType == model.PropertyRemoved ||
+						(change.Property == "required" && change.New == "true" && change.Original == "false"))
+
+				if isActuallyBreaking {
+					breaking = append(breaking, changeDesc)
+				}
+
+				switch change.ChangeType {
+				case model.ObjectAdded, model.PropertyAdded:
+					added = append(added, changeDesc)
+				case model.Modified:
+					modified = append(modified, changeDesc)
+				case model.ObjectRemoved, model.PropertyRemoved:
+					removed = append(removed, changeDesc)
+				}
+			}
+		}
+
+		// Process any remaining changes without specific context
+		allChanges := c.changes.GetAllChanges()
 		for _, change := range allChanges {
+			// Create unique key for deduplication based on change properties
+			changeKey := fmt.Sprintf("%d|%s|%s|%s", change.ChangeType, change.Property, change.Original, change.New)
+
+			// Skip duplicates - these were already processed with proper context
+			if seen[changeKey] {
+				continue
+			}
+			seen[changeKey] = true
+
 			changeDesc := c.formatChange(change)
 
-			if change.Breaking {
+			// Skip empty descriptions
+			if changeDesc == "" {
+				continue
+			}
+
+			// Separate endpoint additions/removals
+			if strings.HasPrefix(changeDesc, "Endpoint ") && strings.Contains(changeDesc, " added") {
+				endpointAdditions = append(endpointAdditions, changeDesc)
+				continue
+			}
+			if strings.HasPrefix(changeDesc, "Endpoint ") && strings.Contains(changeDesc, " removed") {
+				endpointRemovals = append(endpointRemovals, changeDesc)
+				breaking = append(breaking, changeDesc)
+				continue
+			}
+
+			// Only mark truly breaking changes as breaking
+			isActuallyBreaking := change.Breaking &&
+				(change.ChangeType == model.ObjectRemoved ||
+					change.ChangeType == model.PropertyRemoved ||
+					(change.Property == "required" && change.New == "true" && change.Original == "false"))
+
+			if isActuallyBreaking {
 				breaking = append(breaking, changeDesc)
 			}
 
@@ -125,6 +429,8 @@ func (c *Comparer) GenerateChangelog() error {
 				removed = append(removed, changeDesc)
 			}
 		}
+
+		// Show all endpoints - no truncation
 
 		file, err := os.Create(c.outputPath)
 		if err != nil {
@@ -139,7 +445,14 @@ func (c *Comparer) GenerateChangelog() error {
 
 		// Write summary
 		fmt.Fprintf(w, "**Total Changes:** %d\n", c.totalChanges)
-		fmt.Fprintf(w, "**Breaking Changes:** %d\n\n", c.changes.TotalBreakingChanges())
+		fmt.Fprintf(w, "**Breaking Changes:** %d\n", len(breaking))
+		if len(endpointAdditions) > 0 {
+			fmt.Fprintf(w, "**Endpoints Added:** %d\n", len(endpointAdditions))
+		}
+		if len(endpointRemovals) > 0 {
+			fmt.Fprintf(w, "**Endpoints Removed:** %d\n", len(endpointRemovals))
+		}
+		fmt.Fprintln(w)
 
 		// Write breaking changes section if any
 		if len(breaking) > 0 {
@@ -150,7 +463,25 @@ func (c *Comparer) GenerateChangelog() error {
 			fmt.Fprintln(w)
 		}
 
-		// Write added section
+		// Write endpoint additions section
+		if len(endpointAdditions) > 0 {
+			fmt.Fprintln(w, "### 🆕 New Endpoints")
+			for _, change := range endpointAdditions {
+				fmt.Fprintf(w, "- %s\n", change)
+			}
+			fmt.Fprintln(w)
+		}
+
+		// Write endpoint removals section
+		if len(endpointRemovals) > 0 {
+			fmt.Fprintln(w, "### 🗑️ Removed Endpoints")
+			for _, change := range endpointRemovals {
+				fmt.Fprintf(w, "- %s\n", change)
+			}
+			fmt.Fprintln(w)
+		}
+
+		// Write added section (non-endpoint additions)
 		if len(added) > 0 {
 			fmt.Fprintln(w, "### ✨ Added")
 			for _, change := range added {
@@ -168,7 +499,7 @@ func (c *Comparer) GenerateChangelog() error {
 			fmt.Fprintln(w)
 		}
 
-		// Write removed section
+		// Write removed section (non-endpoint removals)
 		if len(removed) > 0 {
 			fmt.Fprintln(w, "### ❌ Removed")
 			for _, change := range removed {
